75b86ee653ebab33facb4dd7a9c3b344852faa27
ssa195 <suaibaamina97@gmail.com>
2025-09-18 16:52:59 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Updated code repository for MammoCLIP-dissect
+#Mammo-CLIP Dissect
+
+"Mammo-CLIP Dissect" is a framework for analyzing the internal representations of deep learning vision models applied to mammography. It extends concept-based interpretability methods to mammography-specific Vision–Language Models (VLMs), enabling researchers to study how convolutional neural networks (CNNs) and VLMs learn clinically meaningful concepts.
+
+This repository contains the code, scripts, and instructions to reproduce the experiments described in our paper:
+
+> Mammo-CLIP Dissect: A Framework for Analysing Mammography Concepts in Vision-Language Models.
--- concept_vit/run_clipdissect.sh
+++ concept_vit/run_clipdissect.sh
@@ -3,7 +3,7 @@
 python /storage/MammoCLIP_copy/src/concept_vit/describe_broad_neurons.py --target_model "breastclip" --target_layers "image_encoder._blocks[0],image_encoder._blocks[1],image_encoder._blocks[2],image_encoder._blocks[3],image_encoder._blocks[4],image_encoder._blocks[5],image_encoder._blocks[6],image_encoder._blocks[7],image_encoder._blocks[8],image_encoder._blocks[9],image_encoder._blocks[10],image_encoder._blocks[11], \
  image_encoder._blocks[12],image_encoder._blocks[13],image_encoder._blocks[14],image_encoder._blocks[15],image_encoder._blocks[16],image_encoder._blocks[17],image_encoder._blocks[18],image_encoder._blocks[19],image_encoder._blocks[20],image_encoder._blocks[21],image_encoder._blocks[22],image_encoder._blocks[23], \
  image_encoder._blocks[24],image_encoder._blocks[25],image_encoder._blocks[26],image_encoder._blocks[27],image_encoder._blocks[28],image_encoder._blocks[29],image_encoder._blocks[30],image_encoder._blocks[31],image_encoder._blocks[32],image_encoder._blocks[33],image_encoder._blocks[34],image_encoder._blocks[35], \
- image_encoder._blocks[36],image_encoder._blocks[37],image_encoder._blocks[38]" --d_probe "vindr" --concept_set "/storage/MammoCLIP_copy/src/Specific_concepts_sorted.txt" --Breast_clip_chkpt "/storage/BreastCLIP_models/b5-model-best-epoch-7.tar"
+ image_encoder._blocks[36],image_encoder._blocks[37],image_encoder._blocks[38]" --d_probe "vindr" --concept_set "/storage/MammoCLIP_copy/src/Concepts/Specific_concepts_sorted.txt" --Breast_clip_chkpt "/storage/BreastCLIP_models/b5-model-best-epoch-7.tar"
 
 
 #BREAST CLIP CLASSIFIER: DENSITY
